d7f83b0608cde14b105ff8f0be47d51374964d82
zxzhou <zhixuan.zhou@thoughtworks.com>
2021-02-28 21:05:56 +0800

    add decrement button and test

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export class Counter extends Component {
+export default class Counter extends Component {
     static displayName = Counter.name;
 
     constructor(props) {
---
+++ ClientApp/src/components/Counter.test.js
@@ -0,0 +1,20 @@
+﻿import '@testing-library/jest-dom'
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import Counter from './Counter'
+
+it('should increase number after clicking the increment button', () => {
+    render(<Counter />)
+    expect(screen.getByText('0')).toBeInTheDocument()
+    const button = screen.getByText('Increment')
+    fireEvent.click(button)
+    expect(screen.getByText('1')).toBeInTheDocument()
+});
+
+it('should decrease number after clicking the decrement button', () => {
+    render(<Counter />)
+    expect(screen.getByText('0')).toBeInTheDocument()
+    const button = screen.getByText('Decrement')
+    fireEvent.click(button)
+    expect(screen.getByText('-1')).toBeInTheDocument()
+});
