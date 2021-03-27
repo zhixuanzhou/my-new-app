d4ab91745db2360d2c3de71a956f54c426870b04
zxzhou <zhixuan.zhou@thoughtworks.com>
2021-03-27 13:14:42 +0800

    add front end api

--- ClientApp/src/api/item.js
+++ ClientApp/src/api/item.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const ItemApi = {
-    createItem : async data => {
-        // console.log(data);
-        const response = await axios.post('http://localhost:3000/items', data);
-        return response;
-    }
-}
+  createItem: async (data) => {
+    // console.log(data);
+    const response = await axios.post("http://localhost:3000/items", data);
+    return response;
+  },
+};
 
 export default ItemApi;
